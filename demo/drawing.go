47e52bce75746f8130cb8d0c72dfbe97013bfe52
Alex Nichol <unixpickle@gmail.com>
2015-02-08 12:12:25 -0500

    updated drawing example (needs mouse events, though)

--- demo/drawing.go
+++ demo/drawing.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	go openWindow()
+	gogui.RunOnMain(openWindow)
 	gogui.Main(&gogui.AppInfo{Name: "Demo"})
 }
 
@@ -26,47 +26,50 @@ func openWindow() {
 	path := []gogui.MouseEvent{}
 	w.SetMouseDownHandler(func(evt gogui.MouseEvent) {
 		path = []gogui.MouseEvent{evt}
-		drawLines(c, path)
+		c.NeedsUpdate()
 	})
 	w.SetMouseDragHandler(func(evt gogui.MouseEvent) {
 		path = append(path, evt)
-		drawLines(c, path)
+		c.NeedsUpdate()
 	})
 	w.SetMouseMoveHandler(func(evt gogui.MouseEvent) {
-		drawCircle(c, evt)
+		path = []gogui.MouseEvent{evt}
+		c.NeedsUpdate()
 	})
 	w.SetMouseUpHandler(func(evt gogui.MouseEvent) {
-		drawCircle(c, evt)
+		path = []gogui.MouseEvent{evt}
+		c.NeedsUpdate()
+	})
+	
+	c.SetDrawHandler(func(ctx gogui.DrawContext) {
+		drawLines(ctx, path)
 	})
 }
 
-func drawCircle(c gogui.Canvas, evt gogui.MouseEvent) {
+func drawCircle(c gogui.DrawContext, evt gogui.MouseEvent) {
 	c.SetFill(0, 0, 0, 1)
 	c.FillEllipse(gogui.Rect{evt.X - 5, evt.Y - 5, 10, 10})
-	c.Flush()
 }
 
-func drawLines(c gogui.Canvas, evts []gogui.MouseEvent) {
+func drawLines(c gogui.DrawContext, evts []gogui.MouseEvent) {
 	if len(evts) == 0 {
-		panic("Expected at least one event.")
+		return
+	} else if len(evts) == 1 {
+		drawCircle(c, evts[0])
+		return
 	}
 	
-	// Stroke the path
-	if len(evts) > 1 {
-		c.SetStroke(1, 0, 0, 1)
-		c.SetThickness(8)
-		c.BeginPath()
-		c.MoveTo(evts[0].X, evts[0].Y)
-		for i := 1; i < len(evts); i++ {
-			c.LineTo(evts[i].X, evts[i].Y)
-		}
-		c.StrokePath()
+	c.SetStroke(1, 0, 0, 1)
+	c.SetThickness(8)
+	c.BeginPath()
+	c.MoveTo(evts[0].X, evts[0].Y)
+	for i := 1; i < len(evts); i++ {
+		c.LineTo(evts[i].X, evts[i].Y)
 	}
+	c.StrokePath()
 	
 	// Fill the last point
 	evt := evts[len(evts) - 1]
 	c.SetFill(0.82, 0.29, 0.29, 1)
 	c.FillEllipse(gogui.Rect{evt.X - 5, evt.Y - 5, 10, 10})
-	
-	c.Flush()
 }
